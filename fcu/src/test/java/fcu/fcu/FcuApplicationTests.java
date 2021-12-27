a15d2cedd96ef1cb8acb16989daea7ba7d58e7f4
weiyi900809 <71805848+weiyi900809@users.noreply.github.com>
2021-12-27 11:29:31 +0800

    Add files via upload
    first upload

---
+++ fcu/src/test/java/fcu/fcu/FcuApplicationTests.java
@@ -0,0 +1,13 @@
+package fcu.fcu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
